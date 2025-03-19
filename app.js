a90deda6cca6ac1028fb254552390fb03402c7af
xammax777 <7maxsiete@gmail.com>
2025-03-18 21:06:22 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+let amigos = [];
+
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, ingresa un nombre válido.");
+        return;
+    }
+
+    if (!/^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$/.test(nombre)) {
+        alert("No es un nombre válido.");
+        return;
+    }
+
+    if (amigos.includes(nombre)) {
+        alert("Ese nombre ya está en la lista.");
+        return;
+    }
+
+    amigos.push(nombre);
+    actualizarLista();
+    input.value = "";
+}
+
+function actualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; 
+
+    amigos.forEach(nombre => {
+        let li = document.createElement("li");
+        li.textContent = nombre;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("La lista está vacía. Agrega nombres primero.");
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<ul><li>🤫 Tu amigo secreto es: <strong>${amigoSorteado}</strong> 🤫</li></ul>`;
+
+}
